e27c6445f13150cfbeecc701f2330d2a299a41e4
Tynasello <tynasello@gmail.com>
2022-01-30 18:52:49 -0500

    Acrochat First Commit

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+
+const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+})
+
+io.on('connection', (socket) => {
+  socket.on('message', ({ name, message }) => {
+    io.emit('message', { name, message })
+  })
+})
+
+const PORT = process.env.PORT || 3000
+
+httpServer.listen(PORT, () => console.log(`🏃 ${PORT}`))
